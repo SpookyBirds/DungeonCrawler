bb45742f923e697a047d37fd5753219706856b46
SpookyBird <spookybirdsgames@gmail.com>
2018-06-18 16:12:08 +0200

    ADD controller

--- DungeonCrawler/Assets/Scripts/Controller/ControllerPlayer.cs
+++ DungeonCrawler/Assets/Scripts/Controller/ControllerPlayer.cs
@@ -2,23 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class ControllerPlayer : Controller {
 
     public KeyCode attackingKey = KeyCode.Mouse0;
 
     private AttackerPlayer attacker;
 
-    private void Awake()
+    protected override void Awake()
     {
         attacker = GetComponent<AttackerPlayer>();
+
+        enemyTypes = new int[]{
+            (int)Entities.Enemy_NPC
+        };
+
+        base.Awake();
     }
 
-    private void Update()
+    protected override void Update()
     {
         if (Input.GetKeyDown(attackingKey))
         {
             attacker.StartAttack();
         }
 
+        base.Update();
     }
 }
---
+++ DungeonCrawler/Assets/Scripts/Holdables/Weapon.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : Holdable {
+
+    [Tooltip("The Collider used to calculate a hit. If it isn't supplied, the script will search it's gameObject as well as children (in ths order)")]
+    public Collider attackCollider;
+
+    /// <summary>
+    /// If no attackCollider is supplied, the script will search the it's gameObject as well as children (in ths order) for an attackCollider"
+    /// </summary>
+    protected override void Awake()
+    {
+        if (attackCollider == null)
+            attackCollider.GetComponent<Collider>();
+        if (attackCollider == null)
+            attackCollider.GetComponentInChildren<Collider>();
+
+        base.Awake();
+    }
+
+    /// <summary>
+    /// Attack!!
+    /// </summary>
+    public override bool Use()
+    {
+
+
+        return base.Use();
+    }
+}
