b2165eb0b94debd3a33a318102e0414ebae8cb44
DESKTOP-MPOD5MS\Alexander <TurinAlex@web.de>
2018-06-19 10:57:52 +0200

    ADD run and block animation

---
+++ DungeonCrawler/Assets/Scripts/Charcter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Charcter : MonoBehaviour {
+
+    private Animator anim;
+    public KeyCode leftUseKey = KeyCode.Mouse0;
+    public KeyCode rightUseKey = KeyCode.Mouse1;
+    public KeyCode runKey = KeyCode.W;
+
+
+    void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	
+	void Update () {
+        anim.SetBool("LeftUse", Input.GetKey(leftUseKey));
+        anim.SetBool("RightUse", Input.GetKeyDown(rightUseKey));
+        anim.SetBool("Run", Input.GetKey(runKey));
+	}
+}
