c8bde9de050e00e2e708c734488410b91b2e6408
SpookyBird <spookybirdsgames@gmail.com>
2018-06-19 11:01:57 +0200

    UPD holdable and entity

--- DungeonCrawler/Assets/Scripts/Holdables/Holdable.cs
+++ DungeonCrawler/Assets/Scripts/Holdables/Holdable.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class Holdable : InheritanceSimplyfier {
 
+    private bool alreadyUsing = false;
+    public bool AlreadyUsing
+    {
+        get { return alreadyUsing; }
+        protected set { alreadyUsing = value; }
+    }
+
     /// <summary>
     /// Activates the use action. Returns whether it was successfull
     /// </summary>
-    public virtual bool Use()
+    public virtual bool Use(Controller controller)
     {
         return false;
     }
