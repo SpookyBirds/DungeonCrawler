9f3be8182277a806951dff88843a2ba860225cb1
SpookyBird <spookybirdsgames@gmail.com>
2018-06-20 12:03:09 +0200

    ADD equipmentHolder

---
+++ DungeonCrawler/Assets/Scripts/Entitys/EquipmetHolder.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipmetHolder : MonoBehaviour {
+
+    public Holdable leftHand;
+    public Holdable rightHand;
+}
