4d61c40f87841aebaed2b6441ccb555c873ed08e
DESKTOP-MPOD5MS\Alexander <TurinAlex@web.de>
2018-06-26 09:41:27 +0200

    UPD blend tree test

---
+++ DungeonCrawler/Assets/RawAssets/Player/rootMotion_character/character_movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character_movement : MonoBehaviour {
+
+    private Animator anim;
+    private Rigidbody rigid;
+    public float horizontalInput;
+    public float verticalInput;
+
+    public Vector3 ForwardDirection { get { return transform.forward; } }
+    public Vector3 LeftDirection { get { return -transform.right; } }
+    public Vector3 BackDirection { get { return -transform.forward; } }
+    public Vector3 RightDirection { get { return transform.right; } }
+
+    void Start () {
+        anim = GetComponentInChildren<Animator>();
+        rigid = GetComponent<Rigidbody>();
+	}
+	
+	
+	void Update () {
+
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        anim.SetBool("running", verticalInput != 0 || horizontalInput != 0);
+
+        rigid.AddForce(ForwardDirection * verticalInput);
+        rigid.AddForce(LeftDirection * horizontalInput);
+
+        anim.SetFloat("verticalVelocity", verticalInput);
+        anim.SetFloat("horizontalVelocity", horizontalInput);
+
+	}
+}
