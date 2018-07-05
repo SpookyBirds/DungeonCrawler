cb401445772ee5d5d923ae1bbf6c667dc2dcc0c6
DESKTOP-MPOD5MS\Alexander <TurinAlex@web.de>
2018-07-05 09:23:03 +0200

    UPD animator

---
+++ DungeonCrawler/Assets/RawAssets/Player/Character v2 Mesh and Animations/Cloth_Test.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloth_Test : MonoBehaviour
+{
+
+    private Animator anim;
+    public float horizontalInput;
+    public float verticalInput;
+   
+   
+
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
+
+    void Update()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+
+}
