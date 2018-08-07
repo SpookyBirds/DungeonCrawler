453ceecbc345fc933b3c1e2b64ebc5dc210dd338
DESKTOP-MPOD5MS\Alexander <TurinAlex@web.de>
2018-08-07 09:18:21 +0200

    ADD new character locomotion animations

---
+++ DungeonCrawler/Assets/RawAssets/Player/NewCharacter/Test_character.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_character : MonoBehaviour {
+
+    Animator anim;
+    bool runForward;
+    float verticalInput;
+    float horizontalInput;
+
+	void Start () {
+        anim = GetComponentInChildren<Animator>();
+	}
+	
+
+	void Update () {
+
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        anim.SetFloat("verticalInput", verticalInput);
+        anim.SetFloat("horizontalInput", horizontalInput);
+
+        anim.SetBool("runForward", verticalInput > 0 || horizontalInput != 0 && verticalInput == 0);
+        anim.SetBool("runBackward", verticalInput < 0);
+        anim.SetBool("Testbool", Input.GetKeyDown(KeyCode.LeftControl));
+
+    }
+}
