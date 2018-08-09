24f52385ca69f71e21479e76cc4fa8a085102d13
DESKTOP-MPOD5MS\Alexander <TurinAlex@web.de>
2018-08-09 11:50:02 +0200

    UPD player

---
+++ DungeonCrawler/Assets/Scripts/PlayerGUIEnabler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGUIEnabler : MonoBehaviour {
+
+    GameObject playerGUI;
+    void Start()
+    {
+        playerGUI = GameObject.Find("PlayerGUI");
+        playerGUI.SetActive(true);
+    }
+	
+
+}
