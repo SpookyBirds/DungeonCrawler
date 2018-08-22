5d61e49aae325f25550e4c813b9f9742756a8d3c
DESKTOP-MPOD5MS\Alexander <TurinAlex@web.de>
2018-08-22 09:23:09 +0200

    UPD substance manager

---
+++ DungeonCrawler/Assets/Scripts/SubstanceVial.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SubstanceVial : MonoBehaviour {
+   
+    [SerializeField] [Tooltip("Bar of Substance")]
+    private Image substanceBar;
+
+    [SerializeField] [Tooltip("fillCapacity")]
+    private int fillCapacity;
+
+    [SerializeField] [Tooltip("amount of current")]
+    private int currentAmount;
+    public int CurrentAmount
+    {
+        get { return currentAmount; }
+
+        private set
+        {
+            currentAmount = value;
+            UpdateFillAmountDisplay();
+        }
+    }
+
+    [SerializeField] [Tooltip("currentSubstance")]
+    private Substance currentSubstance;
+    public Substance CurrentSubstance
+    {
+        get { return currentSubstance; }
+
+        private set
+        {
+            currentSubstance = value;
+            UpdateSubstanceDisplay();
+        }
+    }
+
+    private void UpdateSubstanceDisplay()
+    {
+
+    }
+
+    private void UpdateFillAmountDisplay()
+    {
+        substanceBar.fillAmount = currentAmount / fillCapacity;
+    }
+
+    public int AddAmount(int amount)
+    {
+        if (amount <= 0)
+            return 0;
+
+        int currentAmountBeforeAdding = CurrentAmount;
+        if((CurrentAmount += amount) > fillCapacity)
+        {
+            CurrentAmount = fillCapacity;
+            return amount - (CurrentAmount - currentAmountBeforeAdding);
+        }
+
+        return 0;
+    }
+
+    public bool RemoveAmount(int amount)
+    {
+        if (amount <= 0)
+            return false;
+
+        return false;
+    }
+
+
+}
---
+++ DungeonCrawler/Assets/Scripts/UISubstanceManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISubstanceManager : MonoBehaviour {
+
+    [SerializeField] [Tooltip("Amount of green Substance")]
+    private int greenSubstanceAmount = 100;
+
+    [SerializeField] [Tooltip("Amount of red Substance")]
+    private int redSubstanceAmount = 100;
+
+    [SerializeField] [Tooltip("Amount of silver Substance")]
+    private int silverSubstanceAmount = 100;
+
+    [SerializeField]
+    private [] number;
+
+ 
+}
